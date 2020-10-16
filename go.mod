34c586ecafd46729b7091cc9af12614f61aab399
Arya Sukma Wijaya <aryasukma.work@gmail.com>
2020-10-16 22:23:30 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/aryasukmawijaya/app-say-hello
+
+go 1.15
+
+require github.com/aryasukmawijaya/go-say-hello v1.5.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/aryasukmawijaya/go-say-hello"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
